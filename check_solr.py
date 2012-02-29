233cc783293ce0cc097c3df2c3c212c1db8c8b22
Chris Ganderton <github@thefraggle.com>
2012-02-29 07:58:56 -0500

    Changes:     compare generation vs replicatableGeneration, means one less json/http call per core query     some code style changes, when handling exceptions, very minor...     change how we handle how and when to alert, in main()
        New Features:
        -w / -c : what int value to warn/crit at when checking the difference between local generation and replicatableGeneration
        -i      : ignore a core, specify multiple times to ignore multiple cores.

--- check_solr.py
+++ check_solr.py
@@ -1,28 +1,24 @@
-#!/usr/bin/env python26
+#!/usr/bin/env python
 '''
-check_solr.py - v0.1 -  Chris Ganderton <github@thefraggle.com>
+check_solr.py - v0.2 -  Chris Ganderton <github@thefraggle.com>
 
-Nagios check script for checking replication issues on solr slaves.
+Nagios check script for checking replication issues and ping status on solr slaves.
 
-We simply get the index version that the core reports it has, and 
-then query the maximum possible index version the master has told the core about.
+We simply get the local generation that the core reports it has, and 
+then query the maximum possible replicateableGeneration the master has told the core about.
 
 OPTIONS:
 
 -H : hostname/ip of the solr server we want to query
 -p : tcp port solr is listening on
 -w : webapp path
+-P : ping the solr cores on given webapp (not to be used with replication check)
+-r : check replication on the given webapp (not to be used with ping check)
+-w : delta between master and local replication version, to warn on (default 1)
+-c : delta between master and local replication version, to crit on (defualt 2)
+-i : ignore a core, use multiple times to ignore multiple cores.
 
-EXAMPLE: ./check_solr_rep.py -H localhost -p 8093 -w webapp -r -P
-
-'''
-'''
-    we could think about using timesFailed, but we need to be sure we can reset this once we know about errors and they're resolved...
-
-    if st.get('timesFailed') != None:
-        print "CRIT: replication failure"
-    else:
-        print "OK: No replication errors"
+EXAMPLE: ./check_solr_rep.py -H localhost -p 8093 -w solr -r -w 10 -c 20
 
 '''
 import urllib, json, sys
@@ -42,23 +38,27 @@ def listcores():
 
     return cores
 
-def corerepstatus(core):
+def repstatus(core):
     rep_cmd     = baseurl + core + '/replication?' + urllib.urlencode({'command':'details','wt':'json'})
-    status_cmd  = baseurl + core_admin_url + urllib.urlencode({'action':'status','wt':'json','core':core})
-
-    ares        = urllib.urlopen(status_cmd)
-    adata       = json.loads(ares.read())
 
     rres        = urllib.urlopen(rep_cmd)
     rdata       = json.loads(rres.read())
 
-    curind      = adata['status'][core]['index'].get('version')
-    availind    = rdata['details']['slave']['masterDetails']['master'].get('replicatableIndexVersion')
+    localgeneration  = rdata['details'].get('generation')
+    mastergeneration = rdata['details']['slave']['masterDetails']['master'].get('replicatableGeneration')
 
-    if curind == availind:
-        status = "Ok"
+    if mastergeneration == None or localgeneration == None:
+        status = "CRITICAL"
+        return status
+
+    generationdiff   = mastergeneration - localgeneration
+
+    if generationdiff > threshold_warn:
+        status = "WARNING"
+    elif generationdiff > threshold_crit:
+        status = "CRITICAL"
     else:
-        status = "Error"
+        status = "UNKNOWN"
 
     return status
 
@@ -73,78 +73,106 @@ def solrping(core):
     return status
 
 def main():
-    ''' ought to put this into it's own func ''' 
-    check_replication = False
-    check_ping        = False
+    global baseurl, core_admin_url, threshold_warn, threshold_crit
 
     cmd_parser = OptionParser(version="%prog 0.1")
     cmd_parser.add_option("-H", "--host", type="string", action="store", dest="solr_server", default="localhost", help="SOLR Server address")
     cmd_parser.add_option("-p", "--port", type="string", action="store", dest="solr_server_port", help="SOLR Server port")
-    cmd_parser.add_option("-w", "--webapp", type="string", action="store", dest="solr_server_webapp", help="SOLR Server webapp path")
-    cmd_parser.add_option("-P", "--ping", action="store_true", dest="check_ping", help="SOLR Ping")
-    cmd_parser.add_option("-r", "--replication", action="store_true", dest="check_replication", help="SOLR Replication check")
+    cmd_parser.add_option("-W", "--webapp", type="string", action="store", dest="solr_server_webapp", help="SOLR Server webapp path")
+    cmd_parser.add_option("-P", "--ping", action="store_true", dest="check_ping", help="SOLR Ping", default=False)
+    cmd_parser.add_option("-r", "--replication", action="store_true", dest="check_replication", help="SOLR Replication check", default=False)
+    cmd_parser.add_option("-w", "--warn", type="string", action="store", dest="threshold_warn", help="WARN threshold for replication check", default=1)
+    cmd_parser.add_option("-c", "--critical", type="string", action="store", dest="threshold_crit", help="CRIT threshold for replication check", default=2)
+    cmd_parser.add_option("-i", "--ignore", type="string", action="append", dest="ignore_cores", help="SOLR Cores to ignore", default="")
 
     (cmd_options, cmd_args) = cmd_parser.parse_args()
 
     if not (cmd_options.solr_server and cmd_options.solr_server_port and cmd_options.solr_server_webapp):
         cmd_parser.print_help()
-        sys.exit(3)
-    
-    global baseurl, core_admin_url
+        return(3)
+
+    if not cmd_options.check_replication and not cmd_options.check_ping:
+        print "ERROR: Please specify -r or -P"
+        return(3)
+
+    if ((cmd_options.threshold_warn and not cmd_options.threshold_crit) or (cmd_options.threshold_crit and not cmd_options.threshold_warn)):
+        print "ERROR: Please use -w and -c together."
+        return(3)
+
+    if cmd_options.threshold_crit <= cmd_options.threshold_warn:
+        print "ERROR: the value for (-c|--critical) must be greater than (-w|--warn)"
+        return(3)
 
     solr_server         = cmd_options.solr_server
     solr_server_port    = cmd_options.solr_server_port
     solr_server_webapp  = cmd_options.solr_server_webapp
     check_ping          = cmd_options.check_ping
     check_replication   = cmd_options.check_replication
+    threshold_warn      = cmd_options.threshold_warn
+    threshold_crit      = cmd_options.threshold_crit
+    ignore_cores        = cmd_options.ignore_cores
 
-    baseurl             = 'http://' + solr_server + ':' + solr_server_port + '/' +  solr_server_webapp + '/'
     core_admin_url      = 'admin/cores?'
-    indexerrors         = []
+    baseurl             = 'http://' + solr_server + ':' + solr_server_port + '/' +  solr_server_webapp + '/'
+
+    repwarn             = []
+    repcrit             = []
+
     pingerrors          = []
 
     try:
         cores = listcores()
     except IOError as (errno, strerror):
-        print "CRIT: {0} - {1}".format(errno,strerror)
-        sys.exit(-1)
-    except ValueError, TypeError:
-        print "CRIT: probably couldn't format JSON data, check SOLR is ok"
-        sys.exit(-1)
+        print "CRITICAL: {0} - {1}".format(errno,strerror)
+        return(2)
+    except (ValueError, TypeError):
+        print "CRITICAL: probably couldn't format JSON data, check SOLR is ok"
+        return(3)
     except:
-        print "CRIT: Unknown error"
-        sys.exit(-1)
+        print "CRITICAL: Unknown error" 
+        return(3)
 
+    # XXX: This is ugly...
     try:
         for core in cores:
-            if check_replication == True:
-                if corerepstatus(core) == 'Error':
-                    indexerrors.append(core)
-            if check_ping == True:
+            if core in ignore_cores:
+                continue 
+            if check_replication:
+                ret = repstatus(core)
+                if ret == 'CRITICAL':
+                    repcrit.append(core)
+                elif ret == 'WARNING':
+                    repwarn.append(core)
+            if check_ping:
                 if solrping(core) != 'OK':
                     pingerrors.append(core)
     except IOError as (errno, strerror):
-        print "CRIT: {0} {1} ".format(errno, strerror)
-        sys.exit(-1)
+        print "CRITICAL: {0} {1} ".format(errno, strerror)
+        return(2)
     except KeyError as strerror:
         if 'slave' in strerror: 
-            print "CRIT: This doesn't seem to be a slave, are you sure you meant to call -r?"
-            sys.exit(-1)
+            print "CRITCAL: This doesn't seem to be a slave, are you sure you meant to call -r?"
+            return(2)
         else:
-            print "CRIT: unknown error (error string: {0})".format(strerror)
+            print "CRITICAL: unknown error (error string: {0})".format(strerror)
             print strerror
-            sys.exit(-1)
+            return(3)
     
-    if not indexerrors and not pingerrors:
-        print "OK: no issues"
-        return 0
-    elif pingerrors:
-        print "CRIT: error pinging core(s) - ",
+    if pingerrors:
+        print "CRITICAL: error pinging core(s) - ",
         print ", ".join(pingerrors)
-        return -1
-    elif indexerrors:
-        print "WARN: replication errors on core(s) -",
-        print ", ".join(indexerrors)
+        return(2)
+    elif repcrit:
+        print "CRITICAL: replication errors on core(s) -",
+        print ", ".join(repcrit)
+        return(2)
+    elif repwarn:
+        print "WARNING: replication errors on core(s) -",
+        print ", ".join(repwarn)
+        return(1)
+    else:
+        print "OK: no issues"
+        return(0)
 
 if __name__ == '__main__':
     sys.exit(main())
